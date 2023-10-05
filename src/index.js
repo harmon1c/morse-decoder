7b9c2cc8dbd2674d8997db53fbffe0bdbb835714
harmon1c <80057115+harmon1c@users.noreply.github.com>
2023-10-05 17:43:06 +0300

    feat: add solution 2023-10-05 17:42:55

--- src/index.js
+++ src/index.js
@@ -36,9 +36,30 @@ const MORSE_TABLE = {
     '----.':  '9',
     '-----':  '0',
 };
-
 function decode(expr) {
-    // write your solution here
+    const morseWords = expr.match(/.{10}/g);
+    let decodedSentence = '';
+
+    for (const morseWord of morseWords) {
+        if (morseWord === '**********') {
+            decodedSentence += ' ';
+        } else {
+            let morseLetter = '';
+
+            for (let i = 0; i < 10; i += 2) {
+                const symbol = morseWord.slice(i, i + 2);
+                if (symbol === '10') {
+                    morseLetter += '.';
+                } else if (symbol === '11') {
+                    morseLetter += '-';
+                }
+            }
+
+            decodedSentence += MORSE_TABLE[morseLetter];
+        }
+    }
+
+    return decodedSentence;
 }
 
 module.exports = {
